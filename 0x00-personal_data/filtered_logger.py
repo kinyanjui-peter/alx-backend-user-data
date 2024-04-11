de7c2d53169d5bb533e6a84575acc3dbf11e1378
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-04-11 22:00:25 +0300

    personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Script for managing Sensitive Information
+Script for managing sensitive data redaction
 """
 from typing import List
 import re
@@ -8,111 +8,117 @@ import logging
 from os import environ
 import mysql.connector
 
-# Sensitive fields to be sanitized
-SENSITIVE_FIELDS = ("name", "email", "phone", "ssn", "password")
 
-def sanitize_data(fields: List[str], sanitization: str,
-                  message: str, separator: str) -> str:
+# PII fields to be redacted
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """
-    Replaces sensitive data in a message with sanitized values
-    based on the list of fields to sanitize
+    Redacts sensitive information in a message based on specified fields.
 
     Args:
-        fields: list of fields to sanitize
-        sanitization: the value to use for sanitization
-        message: the string message to filter
-        separator: the separator to use between fields
+        fields: List of fields to redact
+        redaction: The value to use for redaction
+        message: The string message to filter
+        separator: The separator to use between fields
 
     Returns:
-        The filtered string message with sanitized values
+        The filtered string message with redacted values
     """
-    for field in fields:
-        message = re.sub(f'{field}=.*?{separator}',
-                         f'{field}={sanitization}{separator}', message)
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
     return message
+
 
 def get_logger() -> logging.Logger:
     """
-    Returns a Logger object for managing Sensitive Information
+    Provides a Logger object configured for redacting sensitive data.
 
     Returns:
-        A Logger object with INFO log level and SanitizingFormatter
-        formatter for sanitizing sensitive fields
+        A Logger object with INFO log level and RedactingFormatter
+        formatter for filtering PII fields
     """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
 
     stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(SanitizingFormatter(list(SENSITIVE_FIELDS)))
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
     logger.addHandler(stream_handler)
 
     return logger
 
-def get_database_connection() -> mysql.connector.connection.MySQLConnection:
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """
-    Returns a MySQLConnection object for accessing Sensitive Information database
+    Retrieves a MySQLConnection object for accessing the database with sensitive data.
 
     Returns:
         A MySQLConnection object using connection details from
         environment variables
     """
-    username = environ.get("SENSITIVE_DATA_DB_USERNAME", "root")
-    password = environ.get("SENSITIVE_DATA_DB_PASSWORD", "")
-    host = environ.get("SENSITIVE_DATA_DB_HOST", "localhost")
-    database_name = environ.get("SENSITIVE_DATA_DB_NAME")
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
 
-    connection = mysql.connector.connection.MySQLConnection(user=username,
+    cnx = mysql.connector.connection.MySQLConnection(user=username,
                                                      password=password,
                                                      host=host,
-                                                     database=database_name)
-    return connection
+                                                     database=db_name)
+    return cnx
+
 
 def main():
     """
-    Main function to retrieve user data from database and log to console
+    Main function to fetch user data from the database and log it securely.
     """
-    db_connection = get_database_connection()
-    cursor = db_connection.cursor()
+    db = get_db()
+    cursor = db.cursor()
     cursor.execute("SELECT * FROM users;")
-    field_names = [column[0] for column in cursor.description]
+    field_names = [i[0] for i in cursor.description]
 
     logger = get_logger()
 
     for row in cursor:
-        str_row = ''.join(f'{field}={str(value)}; ' for value, field in zip(row, field_names))
+        str_row = ''.join(f'{f}={str(r)}; ' for r, f in zip(row, field_names))
         logger.info(str_row.strip())
 
     cursor.close()
-    db_connection.close()
+    db.close()
 
-class SanitizingFormatter(logging.Formatter):
+
+class RedactingFormatter(logging.Formatter):
     """
-    Sanitizing Formatter class for filtering sensitive fields
+    Formatter class for redacting PII fields in log messages.
     """
 
-    SANITIZATION = "***"
-    FORMAT = "[SENSITIVE_DATA] %(name)s %(levelname)s %(asctime)-15s:%(message)s"
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
         """
-        Constructor method for SanitizingFormatter class
+        Constructor method for RedactingFormatter class.
 
         Args:
-            fields: list of fields to sanitize in log messages
+            fields: List of fields to redact in log messages
         """
-        super(SanitizingFormatter, self).__init__(self.FORMAT)
+        super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """
         Formats the specified log record as text.
-        Sanitizes values in incoming log records using sanitize_data.
+
+        Filters values in incoming log records using filter_datum.
         """
-        record.msg = sanitize_data(self.fields, self.SANITIZATION,
-                    record.getMessage(), self.SEPARATOR)
-        return super(SanitizingFormatter, self).format(record)
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
 
 
 if __name__ == '__main__':
