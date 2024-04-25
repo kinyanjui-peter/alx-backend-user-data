029dd3f5fe12abf78f6b9fcbcb5a0950b4409029
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-04-25 12:33:10 +0300

    user model

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -50,5 +50,5 @@ class DB:
     #         raise InvalidRequestError("InvalidRequestError") from e
         # for key, value in kwargs:
         #     find_user = {if }
-# if __name__ = "__main__":
-#     app.run(DB)
+if __name__ = "__main__":
+    my_db = DB()
