4fc8d13993ae0f7331399964c3dbb339be3da151
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-04-25 19:45:44 +0300

    salting

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
-"""authentication"""
+"""authentication
+"""
 import bcrypt
 
 
 def _hash_password(Password: str, ) -> bytes:
-    """ a function that takes in password, and salt and hash it"""
+    """ a function that takes in password, and salt and hash it
+    args:
+        password(str): password to be hashed
+
+    Returns:
+        bytes: hashed password
+    """
     password_in_byte = Password.encode('utf-8')
     hashed_password = bcrypt.hashpw(password_in_byte, bcrypt.gensalt())
     return hashed_password
