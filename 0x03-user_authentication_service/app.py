371223ed380b282ae4e43e7ec4532b4a682117dc
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-04-26 09:46:51 +0300

    registre user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -9,33 +9,38 @@ app = Flask(__name__)
 # Instantiate Auth object
 AUTH = Auth()
 
+
 @app.route('/')
 def Get_payload():
     """Endpoint for the root URL ("/")"""
     return jsonify(message="Bienvenue")
+
 
 @app.route('/users/', methods=['POST'])
 def register_users():
     """Endpoint to register a user"""
     try:
         email = request.form.get('email')
-        password = request.form.get('password')  # Corrected to lowercase 'password'
-        
+        password = request.form.get('password')
+        # Corrected to lowercase 'password'
+
         # Check if user exists in the database
         existing_user = AUTH._db.find_user_by(email=email)
-        
+
         # If user is not registered
         if existing_user is None:
             # Register the user
-            new_user = AUTH.register_user(email=email, password=password)  # Corrected 'password'
+            new_user = AUTH.register_user(email=email, password=password)
+            # Corrected 'password'
             # Return success message
             return jsonify(email=new_user.email, message="user created"), 201
 
         # If user is already registered
         return jsonify(message="email already registered"), 400
-    
+
     except Exception as e:
         return jsonify(message="User could not be registered"), 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
