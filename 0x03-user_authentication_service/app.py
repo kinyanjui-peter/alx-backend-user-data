427981988aa92a1327e00b67688df5a04c761bab
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-04-27 05:44:40 +0300

    Get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -17,7 +17,7 @@ def Get_payload():
 
 
 @app.route('/users/', methods=['POST'])
-def register_users():
+def users() -> str:
     """Endpoint to register a user"""
     try:
         email = request.form.get('email')
@@ -40,6 +40,84 @@ def register_users():
 
     except Exception as e:
         return jsonify(message="User could not be registered"), 500
+    
+
+@app.route('/sessions', methods=['POST'])
+def login() -> str:
+    """_summary_
+
+    Returns:
+        str: _description_
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not (AUTH.valid_login(email, password)):
+        abort(401)
+    else:
+        # create a new session
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+
+    return response
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout() -> str:
+    """_summary_
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """_summary_
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token() -> str:
+    """_summary_
+
+    Returns:
+        str: _description_
+    """
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except Exception:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'])
+def update_password() -> str:
+    """_summary_
+
+    Returns:
+        str: _description_
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except Exception:
+        abort(403)    
+
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,8 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
+rom typing import Union
 
 
 def _hash_password(Password: str, ) -> bytes:
@@ -18,6 +20,18 @@ def _hash_password(Password: str, ) -> bytes:
     password_in_byte = Password.encode('utf-8')
     hashed_password = bcrypt.hashpw(password_in_byte, bcrypt.gensalt())
     return hashed_password
+
+def _generate_uuid() -> str:
+    """_summary_
+
+    Raises:
+        ValueError: description
+
+    Returns:
+        str: description
+    """
+    id = uuid4()
+    return str(id)
 
 
 class Auth:
@@ -39,3 +53,100 @@ class Auth:
         else:
             # if user already exists, throw error
             raise ValueError('User {} already exists'.format(email))
+    
+    def valid_login(self, email: str, password: str) -> bool:
+        """_summary_
+
+        Args:
+            email (str): description
+            password (str): description
+
+        Returns:
+            Boolean: description
+        """
+        try:
+            # find the user with the given email
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        # check validity of password
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+
+    def create_session(self, email: str) -> str:
+        """_summary_
+
+        Args:
+            email (str): description
+
+        Returns:
+            str: description
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        else:
+            user.session_id = _generate_uuid()
+            return user.session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """_summary_
+
+        Args:
+            session_id (_type_): description
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        else:
+            return user
+
+    def destroy_session(self, user_id: str) -> None:
+        """_summary_
+
+        Args:
+            user_id (str): description
+        """
+        try:
+            user = self._db.find_user_by(id=user_id)
+        except NoResultFound:
+            return None
+        else:
+            user.session_id = None
+            return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """_summary_
+
+        Args:
+            email (str): description
+
+        Returns:
+            str: description
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        else:
+            user.reset_token = _generate_uuid()
+            return user.reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """_summary_
+
+        Args:
+            reset_token (str): description
+            password (str): description
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        else:
+            user.hashed_password = _hash_password(password)
+            user.reset_token = None
+            return None
